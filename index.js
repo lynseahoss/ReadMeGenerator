979f83af543da12e7d3ef5f37c3c38c7782a5856
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-05-21 19:00:24 -0400

    added files to start ReadMe

---
+++ index.js
@@ -0,0 +1,42 @@
+const inquirer = require("inquirer")
+const axios = require("axios");
+const pdf = require("html-pdf");
+const js = require("./utils/generateMarkdown.js")
+
+// one dot = look in same folder currently in  for the wanted js file (generateMarkDown.js)
+//two dots -- brings/moves up/back one directory/folder to look for js file 
+//{}= makes an object (key:value)
+
+const questions = [
+{
+    name: "username",
+    type: "input",
+    message: "Enter your username",
+
+},
+{
+    name: "color",
+    type: "list",
+    message: "Choose your fave color",
+    choices: ["pink","purple", "blue"]
+}
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+//uses inquirer to get info needed- calls to gitH to get users profile back
+//parameters are what go into ()s of a function to let developers know what the function expects
+    inquirer.prompt(questions).then(function(answer){
+        //promise function
+        axios.get() 
+        //github api inside of .get()s githubURL + username (find out how to access github api username)
+        .then(res)
+        //.then gives info back
+        writeToFile()
+    })
+}
+
+init();
+//res gives resolution
