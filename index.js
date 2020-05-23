90f2433e464f47af08e789e7c39a1fc4d5b2778c
Lindsey Lawson <lynseahoss@gmail.com>
2020-05-23 11:13:00 -0400

    removed color, restarting the function init

--- index.js
+++ index.js
@@ -58,10 +58,6 @@ const questions = [
     message: "Enter any questions for the project"
 },
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 47cc0248018e8456a02dd6193bf2778fa09cbb95
 ];
 
 
@@ -72,19 +68,8 @@ function init() {
         axios.get(`https://api.github.com/users/${answer.username}`) 
         //github api inside of .get()s githubURL + username (find out how to access github api username)
         .then(res=> {
-<<<<<<< HEAD
             console.log(encodeURI(answer.license))
            
-=======
-           
-            res.data.title = answer.title
-            res.data.description = answer.description
-            res.data.install = answer.install
-            res.data.usage = answer.usage
-            res.data.contributing = answer.contributing
-            res.data.test = answer.test
-            res.data.quest1 = answer.quest1 
->>>>>>> 47cc0248018e8456a02dd6193bf2778fa09cbb95
 
         })
        
